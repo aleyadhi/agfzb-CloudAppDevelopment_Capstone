b48a4d02eb95346bbaefc2e231397b072072d20d
aleyadhi <aleyadhi@hotmail.com>
2022-08-25 19:16:25 +0000

    adding new models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,16 +4,17 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=1000)
 
+def __str__(self):
+    return "CarMake: " + slef.name + "," + \
+            "Description: " + self.description
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
+class CarModel(models.Model):
+    carmake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
 # - Dealer id, used to refer a dealer created in cloudant database
 # - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
 # - Year (DateField)
